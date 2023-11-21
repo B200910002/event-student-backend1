124803145d95ba2334d625bfc5fd46dba8c8f3f6
Jamts Budee <jamts.b@nomadicss.mn>
2023-11-22 00:12:41 +0800

    crud event and group

--- routes/event.js
+++ routes/event.js
@@ -5,8 +5,143 @@ const { Event, eventSchema } = require('../models/Event.model');
 
 /* GET listing. */
 router.get('/', async (req, res, next) => {
-    const events = await Event.find();
-    res.send(events);
+    try {
+        const events = await Event.find();
+        res.status(200).json(events);
+    } catch (error) {
+        res.status(400).json({ error: error.message })
+    }
+});
+
+router.get('/:eventId', async (req, res, next) => {
+    try {
+        const { eventId } = req.params;
+        const event = await Event.findById(eventId);
+        res.status(200).json(event);
+    } catch (error) {
+        res.status(400).json({ error: error.message })
+    }
+});
+
+/* POST listing. */
+router.post('/', protect, async (req, res, next) => {
+    try {
+        const { name, description, startAt, endAt, place, students, groups } = req.body;
+        const event = await Event.create({
+            name: name,
+            description: description,
+            startAt: startAt,
+            endAt: endAt,
+            place: place,
+            students: students,
+            groups: groups,
+            createdBy: req.user.id,
+            updatedBy: req.user.id
+        });
+        res.status(200).json(event);
+    } catch (error) {
+        res.status(400).json({ error: error.message });
+    }
+});
+
+/* PUT listing. */
+router.put('/:eventId', protect, async (req, res, next) => {
+    try {
+        const { eventId } = req.params;
+        const { name, description, startAt, endAt, place } = req.body;
+        const event = await Event.findByIdAndUpdate(
+            eventId,
+            { name, description, startAt, endAt, place, updatedBy: req.user.id },
+            { new: true }
+        );
+        res.status(200).json(event);
+    } catch (error) {
+        res.status(400).json({ error: error.message });
+    }
+});
+
+/* PATCH listing. */
+router.patch('/:eventId/add-students', protect, async (req, res, next) => {
+    try {
+        const { eventId } = req.params;
+        const { students } = req.body;
+
+        const studentIds = students.map(student => student._id);
+
+        const event = await Event.findByIdAndUpdate(
+            eventId,
+            { $addToSet: { students: { $each: studentIds } } },
+            { new: true }
+        );
+        res.status(200).json(event);
+    } catch (error) {
+        res.status(400).json({ error: error.message });
+    }
+});
+
+router.patch('/:eventId/remove-students', protect, async (req, res, next) => {
+    try {
+        const { eventId } = req.params;
+        const { students } = req.body;
+
+        const studentsRemove = students.map(student => student._id);
+
+        const event = await Event.findByIdAndUpdate(
+            eventId,
+            { $pull: { students: { $in: studentsRemove } } },
+            { new: true }
+        );
+        res.status(200).json(event);
+    } catch (error) {
+        res.status(400).json({ error: error.message });
+    }
+});
+
+router.patch('/:eventId/add-groups', protect, async (req, res, next) => {
+    try {
+        const { eventId } = req.params;
+        const { groups } = req.body;
+
+        const groupIds = groups.map(group => group._id);
+
+        const event = await Event.findByIdAndUpdate(
+            eventId,
+            { $addToSet: { groups: { $each: groupIds } } },
+            { new: true }
+        );
+        res.status(200).json(event);
+    } catch (error) {
+        res.status(400).json({ error: error.message });
+    }
+});
+
+router.patch('/:eventId/remove-groups', protect, async (req, res, next) => {
+    try {
+        const { eventId } = req.params;
+        const { groups } = req.body;
+
+        const groupsRemove = groups.map(group => group._id);
+
+        const event = await Event.findByIdAndUpdate(
+            eventId,
+            { $pull: { groups: { $in: groupsRemove } } },
+            { new: true }
+        );
+        res.status(200).json(event);
+    } catch (error) {
+        res.status(400).json({ error: error.message });
+    }
+});
+
+/* DELETE listing. */
+router.delete('/:eventId', protect, async (req, res, next) => {
+    try {
+        const { eventId } = req.params;
+        const deletedEvent = await Event.findByIdAndDelete(eventId);
+        res.status(200).json(deletedEvent);
+    } catch (error) {
+        res.status(400).json({ error: error.message });
+    }
 });
 
 module.exports = router;
--- routes/group.js
+++ routes/group.js
@@ -4,42 +4,104 @@ const { protect } = require('../middleware/middleware');
 const { Group, groupSchema } = require('../models/Group.model');
 
 /* GET listing. */
-router.get('/', (req, res, next) => {
-    Group.find().then(r => {
-        res.status(200).json(r);
-    }).catch(e => {
-        res.status(400).json({ error: e.message });
-    });
+router.get('/', async (req, res, next) => {
+    try {
+        const groups = await Group.find();
+        res.status(200).json(groups);
+    } catch (error) {
+        res.status(400).json({ error: error.message });
+    }
+});
+
+router.get('/:groupId', async (req, res, next) => {
+    try {
+        const { groupId } = req.params
+        const group = await Group.findById(groupId);
+        res.status(200).json(group);
+    } catch (error) {
+        res.status(400).json({ error: error.message });
+    }
 });
 
 /* POST listing. */
-router.post('/', protect, (req, res, next) => {
-    const { name, description, students } = req.body;
-    Group.create({
-        name: name,
-        description: description,
-        students: students,
-        createdBy: req.user.id,
-        updatedBy: req.user.id
-    }).then(group => {
+router.post('/', protect, async (req, res, next) => {
+    try {
+        const { name, description, students } = req.body;
+        const group = await Group.create({
+            name: name,
+            description: description,
+            students: students,
+            createdBy: req.user.id,
+            updatedBy: req.user.id
+        });
         res.status(201).json(group);
-    }).catch(error => {
-        res.status(400).json({ error: e.message });
-    });
+    } catch (error) {
+        res.status(400).json({ error: error.message });
+    }
 });
 
-/* POST listing. */
-router.put('/:id', protect, (req, res, next) => {
-    const { id } = req.params;
-    const { name, description } = req.body;
-    Group.findById(id).then(group => {
-        group.name = name;
-        group.description = description;
-        group.save();
-        res.status(202).json(group);
-    }).catch(e => {
-        res.status(400).json({ error: e.message });
-    });
+/* PUT listing. */
+router.put('/:groupId', protect, async (req, res, next) => {
+    try {
+        const { groupId } = req.params;
+        const { name, description } = req.body;
+        const group = await Group.findByIdAndUpdate(
+            groupId,
+            { name, description, updatedBy: req.user.id },
+            { new: true }
+        );
+        res.status(200).json(group);
+    } catch (error) {
+        res.status(400).json({ error: error.message });
+    }
+});
+
+/* PATCH listing. */
+router.patch('/:groupId/add-students', protect, async (req, res, next) => {
+    try {
+        const { groupId } = req.params;
+        const { students } = req.body;
+
+        const studentIds = students.map(student => student._id);
+
+        const group = await Group.findByIdAndUpdate(
+            groupId,
+            { $addToSet: { students: { $each: studentIds } } },
+            { new: true }
+        );
+        res.status(200).json(group);
+    } catch (error) {
+        res.status(400).json({ error: error.message });
+    }
+});
+
+router.patch('/:groupId/remove-students', protect, async (req, res, next) => {
+    try {
+        const { groupId } = req.params;
+        const { students } = req.body;
+
+        const studentsRemove = students.map(student => student._id);
+
+        const group = await Group.findByIdAndUpdate(
+            groupId,
+            { $pull: { students: { $in: studentsRemove } } },
+            { new: true }
+        );
+        res.status(200).json(group);
+    } catch (error) {
+        res.status(400).json({ error: error.message });
+    }
+});
+
+/* DELETE listing. */
+router.delete('/:groupId', protect, async (req, res, next) => {
+    try {
+        const { groupId } = req.params;
+        const deletedGroup = await Group.findByIdAndDelete(groupId);
+        res.status(200).json(deletedGroup);
+    } catch (error) {
+        res.status(400).json({ error: error.message });
+    }
 });
 
 module.exports = router;
